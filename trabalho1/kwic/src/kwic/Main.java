85d86ffde5781ae8e581e1f78a185a5e98f0e59d
Joao Victor <joao.victors1203@gmail.com>
2019-09-30 22:33:16 -0300

    dblp input

--- trabalho1/kwic/src/kwic/Main.java
+++ trabalho1/kwic/src/kwic/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import kwic.base.BaseCircularShift;
 import kwic.base.BaseInput;
+import kwic.base.DBLPInput;
 import kwic.interfaces.*;
 
 public class Main {
@@ -12,14 +13,14 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Input in = new BaseInput("input.txt");
-	
+		//Input in = new BaseInput("input.txt");
+		Input in = new DBLPInput();
+		
 		CircularShift cs = new BaseCircularShift(in.getLines());
 		ArrayList<ArrayList<String>> a = cs.getShiftedWords();
 		for(ArrayList<String> array : a) {
 			System.out.println(array);
 		}
-		
 		
 	}
 	
---
+++ trabalho1/kwic/src/kwic/base/DBLPInput.java
@@ -0,0 +1,60 @@
+package kwic.base;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import kwic.interfaces.Input;
+
+
+public class DBLPInput implements Input {
+
+	ArrayList<String> lines;
+	public DBLPInput() {
+		try {
+			lines = new ArrayList<String>();
+			
+			System.out.println("Insira o criterio de busca (autor, titulo, etc): ");
+			Scanner sc = new Scanner(System.in);
+			String query = sc.nextLine();
+			
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			
+			Document doc = builder.parse("https://dblp.org/search/publ/api?format=xml&q=" + query);
+			NodeList nl = doc.getElementsByTagName("title");
+			for(int i=0; i < nl.getLength(); i++) {
+				Node n = nl.item(i);
+				// remove o ponto final (caso tenha)
+				String r = n.getTextContent();
+				if(r.substring(r.length() - 1).equals(".")) {
+					r = r.substring(0, r.length() - 2);
+				}
+				lines.add(r);
+			}
+			
+		} catch(Exception e) {
+			System.err.printf("Erro ao buscar utilizando o criterio desejado.");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public ArrayList<String> getLines() {
+		// TODO Auto-generated method stub
+		return lines;
+	}
+
+}
